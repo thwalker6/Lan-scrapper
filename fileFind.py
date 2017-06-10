05d097dd38c7295f184173d934e01c5f8caffd96
thwalker6 <thomas.e.walker88@gmail.com>
2017-06-09 20:32:40 -0400

    Add files via upload

---
+++ fileFind.py
@@ -0,0 +1,28 @@
+import os
+import csv
+import time
+import sys
+import datetime	
+timeStarted = datetime.datetime.now()
+started = timeStarted.strftime("%m/%d/%Y %I:%M %p")
+
+mydir = "M:\\"
+bad_words = ['old', 'trash', 'replace', 'delete', '- copy']
+def find_path(MyDire):
+	for root, dirs, files in os.walk(mydir):
+		for filename in files:
+			for bad in bad_words:
+				if bad in filename.lower():
+					file_path = os.path.join(root,filename)
+					
+					file_size = os.path.getsize(file_path)
+
+					print ("\"" + file_path + "\"" + \
+						 ",\"" + str(round(file_size/1000)) + "\"" + \
+						 ",\"" + filename + "\"", file=f);
+				
+with open('C:\\Temp\\LAN Contents ' + timeStarted.strftime("%Y-%m-%d %I.%M %p") + '.csv', 'w', newline='', encoding='utf8') as f:
+	print("\"File Path\",\"Size (KB)\",\"FileName\"", file=f)
+	
+	find_path(mydir)
+	print("Complete")
